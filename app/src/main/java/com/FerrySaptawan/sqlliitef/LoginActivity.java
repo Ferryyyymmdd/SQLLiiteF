b509a4329424e73198386aaa1c72e78a5cef9310
Ferryyyymmdd <galaxygalellei13@gmail.com>
2023-07-13 23:54:20 +0700

    new

--- app/src/main/java/com/FerrySaptawan/sqlliitef/LoginActivity.java
+++ app/src/main/java/com/FerrySaptawan/sqlliitef/LoginActivity.java
@@ -1,76 +1,76 @@
 package com.FerrySaptawan.sqlliitef;
 
-import androidx.appcompat.app.AppCompatActivity;
+    import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+    import android.content.Intent;
+    import android.os.Bundle;
+    import android.text.TextUtils;
+    import android.view.View;
+    import android.widget.Button;
+    import android.widget.EditText;
+    import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+    public class LoginActivity extends AppCompatActivity {
 
-    EditText username, password;
-    Button btnSignIn, btnRegist, btnsecretchamber;
+        EditText username, password;
+        Button btnSignIn, btnRegist, btnsecretchamber;
 
-    DBHelper DB;
+        DBHelper DB;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_login);
 
-        username = findViewById(R.id.username);
-        password = findViewById(R.id.password);
-        btnSignIn = findViewById(R.id.signin);
-        btnRegist = findViewById(R.id.regist);
-        btnsecretchamber =  findViewById(R.id.sc);
+            username = findViewById(R.id.username);
+            password = findViewById(R.id.password);
+            btnSignIn = findViewById(R.id.signin);
+            btnRegist = findViewById(R.id.regist);
+            btnsecretchamber =  findViewById(R.id.sc);
 
-        DB = new DBHelper(this);
+            DB = new DBHelper(this);
 
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String user = username.getText().toString().trim();
-                String pass = password.getText().toString().trim();
+            btnSignIn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String user = username.getText().toString().trim();
+                    String pass = password.getText().toString().trim();
 
-                if (TextUtils.isEmpty(user) || TextUtils.isEmpty(pass)) {
-                    Toast.makeText(LoginActivity.this, "Semua Field Wajib Diisi", Toast.LENGTH_SHORT).show();
-                } else {
-                    boolean checkUserPw = DB.checkUsernamePassword(user, pass);
-                    if (checkUserPw) {
-                        Toast.makeText(LoginActivity.this, "Login Sukses", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                        startActivity(intent);
-                        finish();
+                    if (TextUtils.isEmpty(user) || TextUtils.isEmpty(pass)) {
+                        Toast.makeText(LoginActivity.this, "Semua Field Wajib Diisi", Toast.LENGTH_SHORT).show();
                     } else {
-                        Toast.makeText(LoginActivity.this, "Login Gagal. Username atau Password Salah", Toast.LENGTH_SHORT).show();
+                        boolean checkUserPw = DB.checkUsernamePassword(user, pass);
+                        if (checkUserPw) {
+                            Toast.makeText(LoginActivity.this, "Login Sukses", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Login Gagal. Username atau Password Salah", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 }
-            }
-        });
+            });
 
-        btnRegist.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+            btnRegist.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            });
 
-        btnsecretchamber.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, SecretChamber.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+            btnsecretchamber.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(LoginActivity.this, SecretChamber.class);
+                    startActivity(intent);
+                    finish();
+                }
+            });
 
+        }
     }
-}
 
 
